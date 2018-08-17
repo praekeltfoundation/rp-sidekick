f6c63eeeffd3b51b7275f79bfc826527f8654e5a
nathanbegbie <nathanbegbie@gmail.com>
2018-08-17 11:38:32 +0200

    rp_sidekick/ -> config/
    this adheres to the pattern of keeping base url and django project configuration inside a config directory.
    It should also reduce confusion between rp_sidekick and sidekick.

--- config/settings.py
+++ config/settings.py
@@ -87,11 +87,11 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"  # noqa
-    },
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"
+    },  # noqa
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"  # noqa
-    },
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
+    },  # noqa
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"  # noqa
     },
