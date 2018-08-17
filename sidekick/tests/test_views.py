d99a21c3c205752f02bbadb51d705815f2cef78e
nathanbegbie <nathanbegbie@gmail.com>
2018-08-17 11:07:52 +0200

    add additional test for view variables

--- sidekick/tests/test_views.py
+++ sidekick/tests/test_views.py
@@ -1,4 +1,5 @@
 import json
+from os import environ
 
 from django.urls import reverse
 from rest_framework import status
@@ -19,3 +20,17 @@ class SurveyCheckViewTests(APITestCase):
 
         self.assertTrue("version" in result)
         self.assertEqual(result["version"], None)
+
+    def test_health_endpoint_with_vars(self):
+        environ["MARATHON_APP_ID"] = "marathon-app-id"
+        environ["MARATHON_APP_VERSION"] = "marathon-app-version"
+
+        response = self.client.get(reverse("health"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
+
+        self.assertTrue("id" in result)
+        self.assertEqual(result["id"], environ["MARATHON_APP_ID"])
+
+        self.assertTrue("version" in result)
+        self.assertEqual(result["version"], environ["MARATHON_APP_VERSION"])
