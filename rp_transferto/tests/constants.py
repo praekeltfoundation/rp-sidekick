ae25f359a9613bcc35c62b67313e08602804c5f4
nathanbegbie <nathanbegbie@gmail.com>
2018-08-23 09:11:27 +0200

    test transferto views

---
+++ rp_transferto/tests/constants.py
@@ -0,0 +1,105 @@
+PING_RESPONSE_DICT = {
+    "info_txt": "pong",
+    "authentication_key": "1327027869",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+}
+
+
+MSISDN_INFO_RESPONSE_DICT = {
+    "country": "Malaysia",
+    "countryid": "799",
+    "operator": "Maxis Malaysia",
+    "operatorid": "385",
+    "connection_status": "100",
+    "destination_msisdn": "60172860300",
+    "destination_currency": "MYR",
+    "product_list": "5,10,20,30",
+    "service_fee_list": "0.00,0.00,0.00,0.00",
+    "retail_price_list": "2.10,4.00,7.80,11.60",
+    "wholesale_price_list": "1.95,3.72,7.25,10.79",
+    "authentication_key": "1326965217",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+    "local_info_amount_list": "5.00,10.00,20.00,30.00",
+    "local_info_value_list": "5.00,10.00,20.00,30.00",
+    "local_info_currency": "MYR",
+}
+
+
+RESERVE_ID_RESPONSE_DICT = {
+    "status_code": "0",
+    "reserved_id": "20123457",
+    "error_txt": "Transaction successful",
+    "authentication_key": "1217303174266",
+    "error_code": "0",
+}
+
+TOPUP_RESPONSE_DICT = {
+    "transactionid": "77748538",
+    "msisdn": "69999999999",
+    "destination_msisdn": "60172860300",
+    "country": "Malaysia",
+    "countryid": "799",
+    "operator": "Maxis Malaysia",
+    "originating_currency": "USD",
+    "destination_currency": "MYR",
+    "product_requested": "10",
+    "actual_product_sent": "10",
+    "wholesale_price": "3.39",
+    "service_fee": "0.00",
+    "retail_price": "4.30",
+    "balance": "8.1",
+    "sms_sent": "yes",
+    "sms": "Happy birthday ",
+    "sender_sms": "yes",
+    "sender_text": "",
+    "cid1": " My Text ",
+    "authentication_key": "1326963417",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+    "local_info_value": "10.00",
+    "local_info_amount": "10.00",
+    "local_info_currency": "MYR",
+    "return_timestamp": "2013-03-18 00:00:01",
+    "return_version": "1.06.08",
+    "reference_operator": "",
+    "operatorid": "",
+    "cid2": "",
+    "cid3": "",
+}
+
+
+GET_COUNTRIES_RESPONSE_DICT = {
+    "country": "Afghanistan,Albania,Anguilla,Antigua and Barbuda,Argentina,Armenia,Aruba",
+    "countryid": "661,662,916,668,669,670,671",
+    "authentication_key": "1337662098",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+}
+
+
+GET_OPERATORS_RESPONSE_DICT = {
+    "country": "Indonesia",
+    "countryid": "767",
+    "operator": "Indosat Starone Indonesia,Telkom Flexi Indonesia,Indosat IM3 Indonesia,AAA-TESTING Indone- sia,Esia Bakrie Telecom Indonesia,Indosat Mentari Indonesia,Telkomsel Simpati Indonesia,Three Telecom Indonesia,Telkomsel KartuAS Indonesia,Excelcom Indonesia,Axis Indonesia",
+    "operatorid": "1320,1322,1313,1310,1326,1312,1324,1327,1325,215,1411",
+    "authentication_key": "1337662386",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+}
+
+
+GET_OPERATOR_PRODUCTS_RESPONSE_DICT = {
+    "country": "Indonesia",
+    "countryid": "767",
+    "operator": "Indosat IM3 Indonesia",
+    "operatorid": "1313",
+    "destination_currency": "IDR",
+    "product_list": "5000,10000,20000,25000,50000,100000",
+    "wholesale_price_list": "0.85,1.28,2.98,5.53,11.05",
+    "retail_price_list": "1.00,1.50,3.50,6.50,13.00",
+    "authentication_key": "1337662386",
+    "error_code": "0",
+    "error_txt": "Transaction successful",
+}
--- rp_transferto/tests/test_views.py
+++ rp_transferto/tests/test_views.py
@@ -1,12 +1,105 @@
+import json
+
+from mock import patch
+from unittest.mock import MagicMock
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
+
+from rp_transferto.utils import TransferToClient
+from .constants import (
+    PING_RESPONSE_DICT,
+    MSISDN_INFO_RESPONSE_DICT,
+    RESERVE_ID_RESPONSE_DICT,
+    GET_COUNTRIES_RESPONSE_DICT,
+    GET_OPERATORS_RESPONSE_DICT,
+    GET_OPERATOR_PRODUCTS_RESPONSE_DICT,
+)
+
+fake_ping = MagicMock(return_value=PING_RESPONSE_DICT)
+fake_msisdn_info = MagicMock(return_value=MSISDN_INFO_RESPONSE_DICT)
+fake_reserve_id = MagicMock(return_value=RESERVE_ID_RESPONSE_DICT)
+fake_get_countries = MagicMock(return_value=GET_COUNTRIES_RESPONSE_DICT)
+fake_get_operators = MagicMock(return_value=GET_OPERATORS_RESPONSE_DICT)
+fake_get_operator_products = MagicMock(
+    return_value=GET_OPERATOR_PRODUCTS_RESPONSE_DICT
+)
 
 
-class ProductTests(APITestCase):
-    def test_get_products(self):
-        """
-        Product Endpoint should return json with list of available products
-        """
-        response = self.client.get(reverse("get_products"))
+class TestTransferToViews(APITestCase):
+    def setUp(self):
+        self.api_client = APIClient()
+
+        self.user = User.objects.create_user(
+            "username", "testuser@example.com", "password"
+        )
+        token = Token.objects.create(user=self.user)
+        self.token = token.key
+        self.api_client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+
+    @patch.object(TransferToClient, "ping", fake_ping)
+    def test_ping_view(self):
+        self.assertFalse(fake_ping.called)
+        response = self.api_client.get(reverse("ping"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), PING_RESPONSE_DICT)
+        self.assertTrue(fake_ping.called)
+
+    @patch.object(TransferToClient, "get_misisdn_info", fake_msisdn_info)
+    def test_msisdn_info_view(self):
+        self.assertFalse(fake_ping.called)
+        response = self.api_client.get(
+            reverse("msisdn_info", kwargs={"msisdn": "+27820000001"})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            json.loads(response.content), MSISDN_INFO_RESPONSE_DICT
+        )
+        self.assertTrue(fake_msisdn_info.called)
+
+    @patch.object(TransferToClient, "reserve_id", fake_reserve_id)
+    def test_reserve_id_view(self):
+        self.assertFalse(fake_reserve_id.called)
+        response = self.api_client.get(reverse("reserve_id"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), RESERVE_ID_RESPONSE_DICT)
+        self.assertTrue(fake_reserve_id.called)
+
+    @patch.object(TransferToClient, "get_countries", fake_get_countries)
+    def test_get_countries_view(self):
+        self.assertFalse(fake_get_countries.called)
+        response = self.api_client.get(reverse("get_countries"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            json.loads(response.content), GET_COUNTRIES_RESPONSE_DICT
+        )
+        self.assertTrue(fake_get_countries.called)
+
+    @patch.object(TransferToClient, "get_operators", fake_get_operators)
+    def test_get_operators_view(self):
+        self.assertFalse(fake_get_operators.called)
+        response = self.api_client.get(
+            reverse("get_operators", kwargs={"country_id": 111})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            json.loads(response.content), GET_OPERATORS_RESPONSE_DICT
+        )
+        self.assertTrue(fake_get_operators.called)
+
+    @patch.object(
+        TransferToClient, "get_operator_products", fake_get_operator_products
+    )
+    def test_get_operator_products_view(self):
+        self.assertFalse(fake_get_operator_products.called)
+        response = self.api_client.get(
+            reverse("get_operator_products", kwargs={"operator_id": 222})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            json.loads(response.content), GET_OPERATOR_PRODUCTS_RESPONSE_DICT
+        )
+        self.assertTrue(fake_get_operator_products.called)
