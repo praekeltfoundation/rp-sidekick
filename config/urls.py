a041598e76521f8fba8532aafbb85e9bab3569e9
nathanbegbie <nathanbegbie@gmail.com>
2018-08-30 11:39:09 +0200

    Add documention for API endpoints
    see http://www.django-rest-framework.org/topics/documenting-your-api/

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.documentation import include_docs_urls
+
+
 urlpatterns = [
     path("", include("sidekick.urls"), name="sidekick"),
     path("admin/", admin.site.urls),
     path("redcap/", include("rp_redcap.urls"), name="rp_redcap"),
     path("transferto/", include("rp_transferto.urls"), name="rp_transferto"),
+    path(
+        "docs/api/",
+        include_docs_urls(
+            title="REST API Documentation",
+            authentication_classes=[],
+            permission_classes=[],
+        ),
+    ),
 ]
