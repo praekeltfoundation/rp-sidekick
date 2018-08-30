09e3480e901d58a259ee13cbb6a6a0d1de96dc2f
nathanbegbie <nathanbegbie@gmail.com>
2018-08-30 13:03:26 +0200

    allow only logged-in admin users to view API documentation

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.documentation import include_docs_urls
-
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAdminUser
 
 urlpatterns = [
     path("", include("sidekick.urls"), name="sidekick"),
@@ -13,8 +14,8 @@ urlpatterns = [
         "docs/api/",
         include_docs_urls(
             title="REST API Documentation",
-            authentication_classes=[],
-            permission_classes=[],
+            authentication_classes=[SessionAuthentication],
+            permission_classes=[IsAdminUser],
         ),
     ),
 ]
