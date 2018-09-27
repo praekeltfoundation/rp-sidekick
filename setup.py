7f83d2069914db3d20a262ed1302a9c3534da09d
Erik Harding <erik@praekelt.org>
2018-09-27 15:03:32 +0200

    version bump 1.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rp-sidekick",
-    version="1.0.6",
+    version="1.0.7",
     url="http://github.com/praekeltfoundation/rp-sidekick",
     license="BSD",
     author="Praekelt Foundation",
