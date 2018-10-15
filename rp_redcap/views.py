34c31a90f982dc59eb751a4fbf89939ddc52adbd
Erik Harding <erik@praekelt.org>
2018-10-15 18:08:24 +0200

    rework redcap views

--- rp_redcap/views.py
+++ rp_redcap/views.py
@@ -6,33 +6,33 @@ from .tasks import project_check, patient_data_check
 from .models import Project
 
 
-def _validate_project(project_id, request):
+def _validate_project_start_task(project_id, request, task):
     try:
         project = Project.objects.get(id=project_id)
     except Project.DoesNotExist:
-        return False, status.HTTP_400_BAD_REQUEST
+        return status.HTTP_400_BAD_REQUEST
 
     if not project.org.users.filter(id=request.user.id).exists():
-        return False, status.HTTP_401_UNAUTHORIZED
+        return status.HTTP_401_UNAUTHORIZED
 
-    return project, status.HTTP_202_ACCEPTED
+    task.delay(project.id)
+
+    return status.HTTP_202_ACCEPTED
 
 
 class StartProjectCheckView(APIView):
     def post(self, request, project_id, *args, **kwargs):
-        project, return_status = _validate_project(project_id, request)
-
-        if project:
-            project_check.delay(project.id)
+        return_status = _validate_project_start_task(
+            project_id, request, project_check
+        )
 
         return HttpResponse(status=return_status)
 
 
 class StartPatientDataCheckView(APIView):
     def post(self, request, project_id, *args, **kwargs):
-        project, return_status = _validate_project(project_id, request)
-
-        if project:
-            patient_data_check.delay(project.id)
+        return_status = _validate_project_start_task(
+            project_id, request, patient_data_check
+        )
 
         return HttpResponse(status=return_status)
