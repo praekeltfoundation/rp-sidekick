2e6c6add456cce8f89ea35f83c37ef5de0b4744c
Erik Harding <erik@praekelt.org>
2018-10-23 13:52:08 +0200

    add hospital and patientrecord to admin

--- rp_redcap/admin.py
+++ rp_redcap/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Survey, Project, Contact
+from .models import Survey, Project, Contact, Hospital, PatientRecord
 
 admin.site.register(Project)
 admin.site.register(Survey)
 admin.site.register(Contact)
+admin.site.register(Hospital)
+admin.site.register(PatientRecord)
