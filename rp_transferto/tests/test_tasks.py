1ba4310e361372a5ac2d4151f5dd01ec34b8a730
nathanbegbie <nathanbegbie@gmail.com>
2018-11-05 13:24:50 +0200

    refactor TestTopupDataTask

--- rp_transferto/tests/test_tasks.py
+++ rp_transferto/tests/test_tasks.py
@@ -2,11 +2,8 @@ from mock import patch
 from unittest.mock import MagicMock
 from django.test import TestCase, override_settings
 
-from temba_client.v2 import TembaClient
-
 from rp_transferto.tasks import topup_data
 from rp_transferto.models import MsisdnInformation
-from rp_transferto.utils import TransferToClient, TransferToClient2
 
 from .constants import (
     MSISDN_INFO_RESPONSE_DICT,
@@ -14,21 +11,12 @@ from .constants import (
     POST_TOPUP_DATA_RESPONSE,
 )
 
-fake_get_misisdn_info = MagicMock(return_value=MSISDN_INFO_RESPONSE_DICT)
-fake_get_operator_products = MagicMock(return_value=GET_PRODUCTS_RESPONSE_DICT)
-fake_topup_data = MagicMock(return_value=POST_TOPUP_DATA_RESPONSE)
-fake_get_contacts_query = MagicMock()
-fake_get_contacts_query.first.return_value = {}
-fake_update_contact = MagicMock(return_value={})
 
-
-@patch.object(TransferToClient, "get_misisdn_info", fake_get_misisdn_info)
-@patch.object(
-    TransferToClient2, "get_operator_products", fake_get_operator_products
-)
-@patch.object(TransferToClient2, "topup_data", fake_topup_data)
-@patch.object(TembaClient, "get_contacts", fake_get_contacts_query)
-@patch.object(TembaClient, "update_contact", fake_update_contact)
+@patch("temba_client.v2.TembaClient.update_contact")
+@patch("temba_client.v2.TembaClient.get_contacts")
+@patch("rp_transferto.utils.TransferToClient2.topup_data")
+@patch("rp_transferto.utils.TransferToClient2.get_operator_products")
+@patch("rp_transferto.utils.TransferToClient.get_misisdn_info")
 @override_settings(
     TRANSFERTO_LOGIN="fake_transferto_login",
     TRANSFERTO_TOKEN="fake_transferto_token",
@@ -36,36 +24,67 @@ fake_update_contact = MagicMock(return_value={})
     TRANSFERTO_APISECRET="fake_transferto_apisecret",
 )
 class TestTopupDataTask(TestCase):
-    def setUp(self):
-        pass
+    def test_successsful_run(
+        self,
+        fake_get_misisdn_info,
+        fake_get_operator_products,
+        fake_topup_data,
+        fake_get_contacts,
+        fake_update_contact,
+    ):
+        fake_get_misisdn_info.return_value = MSISDN_INFO_RESPONSE_DICT
+        fake_get_operator_products.return_value = GET_PRODUCTS_RESPONSE_DICT
+        fake_topup_data.return_value = POST_TOPUP_DATA_RESPONSE
 
-    def test_successsful_run(self):
+        fake_query_obj = MagicMock()
+        fake_query_obj.first.return_value = "fake_contact_object"
+
+        fake_get_contacts.return_value = fake_query_obj
+
         # check nothing has been called
         self.assertFalse(fake_get_misisdn_info.called)
         self.assertFalse(fake_get_operator_products.called)
         self.assertFalse(fake_topup_data.called)
-        self.assertFalse(fake_get_contacts_query.called)
+        self.assertFalse(fake_get_contacts.called)
         self.assertFalse(fake_update_contact.called)
 
-        # run the task
+        # create test variables
         msisdn = "+27820000000"
         user_uuid = "1234-abc"
         recharge_value = "1GB"
+
+        # run the task
         topup_data(msisdn, user_uuid, recharge_value)
 
         # check that functions were called
         self.assertTrue(fake_get_misisdn_info.called)
         self.assertTrue(fake_get_operator_products.called)
         self.assertTrue(fake_topup_data.called)
-        self.assertTrue(fake_get_contacts_query.called)
+        self.assertTrue(fake_get_contacts.called)
         self.assertTrue(fake_update_contact.called)
 
-    def test_successsful_run_with_cached_msisdn(self):
+    def test_successsful_run_with_cached_msisdn(
+        self,
+        fake_get_misisdn_info,
+        fake_get_operator_products,
+        fake_topup_data,
+        fake_get_contacts,
+        fake_update_contact,
+    ):
+        fake_get_misisdn_info.return_value = MSISDN_INFO_RESPONSE_DICT
+        fake_get_operator_products.return_value = GET_PRODUCTS_RESPONSE_DICT
+        fake_topup_data.return_value = POST_TOPUP_DATA_RESPONSE
+
+        fake_query_obj = MagicMock()
+        fake_query_obj.first.return_value = "fake_contact_object"
+
+        fake_get_contacts.return_value = fake_query_obj
+
         # check nothing has been called
         self.assertFalse(fake_get_misisdn_info.called)
         self.assertFalse(fake_get_operator_products.called)
         self.assertFalse(fake_topup_data.called)
-        self.assertFalse(fake_get_contacts_query.called)
+        self.assertFalse(fake_get_contacts.called)
         self.assertFalse(fake_update_contact.called)
 
         # create test variables
@@ -85,5 +104,5 @@ class TestTopupDataTask(TestCase):
         # check that functions were called
         self.assertTrue(fake_get_operator_products.called)
         self.assertTrue(fake_topup_data.called)
-        self.assertTrue(fake_get_contacts_query.called)
+        self.assertTrue(fake_get_contacts.called)
         self.assertTrue(fake_update_contact.called)
