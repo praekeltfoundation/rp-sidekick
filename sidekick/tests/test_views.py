d9d0b2db266860a587c9d3014b47ad8211b8d7c8
nathanbegbie <nathanbegbie@gmail.com>
2019-02-20 13:19:12 +0200

    write failing tests for check contact view
    TDD FTW

--- sidekick/tests/test_views.py
+++ sidekick/tests/test_views.py
@@ -4,42 +4,24 @@ from os import environ
 from urllib.parse import urlencode
 
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 
 from ..models import Organization
 
 
-class SidekickViewTests(APITestCase):
+FAKE_ENGAGE_URL = "http://localhost:8005"
+
+
+class HealthViewTest(APITestCase):
     def setUp(self):
-        self.client = APIClient()
-
-    def add_whatsapp_messages_200_response(self, responses):
-        """
-        Keep things DRY by reusing this snippet when testing WA response
-        """
-        responses.add(
-            responses.POST,
-            "http://localhost:8005/v1/messages",
-            json={
-                "messages": [{"id": "sdkjfgksjfgoksdflgs"}],
-                "meta": {"api_status": "stable", "version": "2.19.4"},
-            },
-            status=201,
-            match_querystring=True,
-        )
-
-    def mk_org(self):
-        return Organization.objects.create(
-            name="Test Organization",
-            url="http://localhost:8002/",
-            token="REPLACEME",
-            engage_url="http://localhost:8005",
-            engage_token="REPLACEME",
-        )
+        self.api_client = APIClient()
 
     def test_health_endpoint(self):
-        response = self.client.get(reverse("health"))
+        response = self.api_client.get(reverse("health"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         result = json.loads(response.content)
 
@@ -53,7 +35,7 @@ class SidekickViewTests(APITestCase):
         environ["MARATHON_APP_ID"] = "marathon-app-id"
         environ["MARATHON_APP_VERSION"] = "marathon-app-version"
 
-        response = self.client.get(reverse("health"))
+        response = self.api_client.get(reverse("health"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         result = json.loads(response.content)
 
@@ -62,6 +44,44 @@ class SidekickViewTests(APITestCase):
 
         self.assertTrue("version" in result)
         self.assertEqual(result["version"], environ["MARATHON_APP_VERSION"])
+
+
+class SidekickAPITestCase(APITestCase):
+    def setUp(self):
+        self.api_client = APIClient()
+
+        self.user = get_user_model().objects.create_superuser(
+            username="superuser", email="superuser@email.com", password="pass"
+        )
+        token = Token.objects.create(user=self.user)
+        self.token = token.key
+        self.api_client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+
+    def mk_org(self):
+        return Organization.objects.create(
+            name="Test Organization",
+            url="http://localhost:8002/",
+            token="REPLACEME",
+            engage_url=FAKE_ENGAGE_URL,
+            engage_token="REPLACEME",
+        )
+
+
+class TestSendTemplateView(SidekickAPITestCase):
+    def add_whatsapp_messages_200_response(self, responses):
+        """
+        Keep things DRY by reusing this snippet when testing WA response
+        """
+        responses.add(
+            responses.POST,
+            "{}/v1/messages".format(FAKE_ENGAGE_URL),
+            json={
+                "messages": [{"id": "sdkjfgksjfgoksdflgs"}],
+                "meta": {"api_status": "stable", "version": "2.19.4"},
+            },
+            status=201,
+            match_querystring=True,
+        )
 
     @responses.activate
     def test_send_wa_template_message_success_no_params(self):
@@ -77,7 +97,7 @@ class SidekickViewTests(APITestCase):
 
         url = "{}?{}".format(reverse("send_template"), urlencode(params))
 
-        response = self.client.get(url)
+        response = self.api_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         request_body = json.loads(responses.calls[0].request.body)
@@ -111,7 +131,7 @@ class SidekickViewTests(APITestCase):
 
         url = "{}?{}".format(reverse("send_template"), urlencode(params))
 
-        response = self.client.get(url)
+        response = self.api_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         request_body = json.loads(responses.calls[0].request.body)
@@ -143,7 +163,7 @@ class SidekickViewTests(APITestCase):
 
         url = "{}?{}".format(reverse("send_template"), urlencode(params))
 
-        response = self.client.get(url)
+        response = self.api_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         content = json.loads(response.content)
         self.assertTrue("error" in content)
@@ -161,8 +181,108 @@ class SidekickViewTests(APITestCase):
 
         url = "{}?{}".format(reverse("send_template"), urlencode(params))
 
-        response = self.client.get(url)
+        response = self.api_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         content = json.loads(response.content)
         self.assertTrue("error" in content)
         self.assertEquals(content["error"], "Missing fields: element_name")
+
+
+class TestCheckContactView(SidekickAPITestCase):
+    @responses.activate
+    def test_wa_check_contact_valid(self):
+        # set up
+        org = self.mk_org()
+        telephone_number = "+27820000001"
+
+        responses.add(
+            responses.POST,
+            "{}/v1/contacts".format(FAKE_ENGAGE_URL),
+            json={
+                "contacts": [
+                    {
+                        "input": telephone_number,
+                        "status": "valid",
+                        "wa_id": telephone_number.replace("+", ""),
+                    }
+                ]
+            },
+            status=201,
+            match_querystring=True,
+        )
+
+        # get result
+        response = self.api_client.get(
+            reverse(
+                "check_contact",
+                kwargs={"org_id": org.id, "msisdn": telephone_number},
+            )
+        )
+
+        # inspect request to Turn
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        turn_request_body = json.loads(responses.calls[0].request.body)
+        self.assertEqual(
+            turn_request_body,
+            {"blocking": "wait", "contacts": [telephone_number]},
+        )
+
+        # inspect response from Sidekick
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertTrue("status" in content)
+        self.assertEquals(content["status"], "valid")
+
+    @responses.activate
+    def test_wa_check_contact_invalid(self):
+        # set up
+        org = self.mk_org()
+        telephone_number = "+27820000001"
+
+        responses.add(
+            responses.POST,
+            "{}/v1/contacts".format(FAKE_ENGAGE_URL),
+            json={
+                "contacts": [{"input": telephone_number, "status": "invalid"}]
+            },
+            status=201,
+            match_querystring=True,
+        )
+
+        # get result
+        response = self.api_client.get(
+            reverse(
+                "check_contact",
+                kwargs={"org_id": org.id, "msisdn": telephone_number},
+            )
+        )
+
+        # inspect request to Turn
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        turn_request_body = json.loads(responses.calls[0].request.body)
+        self.assertEqual(
+            turn_request_body,
+            {"blocking": "wait", "contacts": [telephone_number]},
+        )
+
+        # inspect response from Sidekick
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertTrue("status" in content)
+        self.assertEquals(content["status"], "invalid")
+
+    def test_wa_check_contact_no_org(self):
+        # get result
+        response = self.api_client.get(
+            reverse(
+                "check_contact", kwargs={"org_id": 99, "msisdn": "16315551003"}
+            )
+        )
+
+        # inspect response from Sidekick
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        content = json.loads(response.content)
+        self.assertTrue("error" in content)
+        self.assertEquals(content["error"], "Organization not found")
