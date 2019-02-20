50abd1bcda02d74706e2388fc16db7d7873321d0
nathanbegbie <nathanbegbie@gmail.com>
2019-02-20 20:28:37 +0200

    Create view to check WhatsApp contact
    - implement view that acts as a wrapper for https://whatsapp.praekelt.org/docs/index.html#templated-messages
    - write docs about endpoint
    - update changelog to include details

--- sidekick/urls.py
+++ sidekick/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("health", views.health, name="health"),
     path("send_template", views.send_wa_template_message, name="send_template"),
+    path(
+        "check_contact/<int:org_id>/<str:msisdn>/",
+        views.CheckContactView.as_view(),
+        name="check_contact",
+    ),
 ]
