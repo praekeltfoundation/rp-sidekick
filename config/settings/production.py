099a96965cbc8085ebf0a53a78b214455817c612
nathanbegbie <nathanbegbie@gmail.com>
2019-03-08 09:40:30 +0200

    Fix sentry env variable
    - it seems that providing False as the default with dotenv provides some unintended consequences, where the value is not pulled through

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,5 +11,5 @@ ALLOWED_HOSTS = env.str("ALLOWED_HOSTS").split(",")
 
 # Configure Sentry Logging
 INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
-RAVEN_DSN = env.str("RAVEN_DSN", False)
+RAVEN_DSN = env.str("RAVEN_DSN", "")
 RAVEN_CONFIG = {"dsn": RAVEN_DSN} if RAVEN_DSN else {}
