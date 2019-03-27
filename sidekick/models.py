79caca0f2d6027422d83b45996d0e63849426285
nathanbegbie <nathanbegbie@gmail.com>
2019-03-27 11:19:37 +0200

    create token on user creation using signals

--- sidekick/models.py
+++ sidekick/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from rest_framework.authtoken.models import Token
 
 from temba_client.v2 import TembaClient
 
@@ -18,3 +22,9 @@ class Organization(models.Model):
 
     def get_rapidpro_client(self):
         return TembaClient(self.url, self.token)
+
+
+@receiver(post_save, sender=User)
+def user_token_creation(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
