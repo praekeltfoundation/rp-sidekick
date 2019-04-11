140110e70fb5f906a585de03d35dea9322d52900
Erik Harding <erik@praekelt.org>
2019-04-11 13:34:00 +0800

    start create group task

--- rp_asos/views.py
+++ rp_asos/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.views import APIView
 
-from .tasks import patient_data_check
-from rp_redcap.views import validate_project_start_task
+from .tasks import patient_data_check, create_hospital_groups
+from rp_redcap.views import validate_project
 
 
 class StartPatientDataCheckView(APIView):
     def post(self, request, project_id, *args, **kwargs):
-        return_status = validate_project_start_task(
-            project_id, request, patient_data_check
-        )
+        return_status = validate_project(project_id, request)
+
+        if return_status == status.HTTP_202_ACCEPTED:
+            chain = patient_data_check.s(project_id) | create_hospital_groups.s(
+                project_id
+            )
+            chain.delay()
 
         return HttpResponse(status=return_status)
