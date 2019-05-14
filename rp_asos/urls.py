2ba0ec374e36f14e71e3be0f7343a319edf376db
Erik Harding <erik@praekelt.org>
2019-05-14 22:20:58 +0800

    asos steering committee notification task

--- rp_asos/urls.py
+++ rp_asos/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import StartPatientDataCheckView
+from .views import StartPatientDataCheckView, StartScreeningRecordCheckView
 
 urlpatterns = [
     path(
         "start-patient-check/<int:project_id>/<slug:tz_code>",
         StartPatientDataCheckView.as_view(),
         name="rp_asos.start_patient_check",
-    )
+    ),
+    path(
+        "start-screening-record-check/<int:org_id>",
+        StartScreeningRecordCheckView.as_view(),
+        name="rp_asos.start_screening_record_check",
+    ),
 ]
--- rp_asos/views.py
+++ rp_asos/views.py
@@ -2,8 +2,25 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.views import APIView
 
-from .tasks import patient_data_check, create_hospital_groups
+from .tasks import (
+    patient_data_check,
+    create_hospital_groups,
+    screening_record_check,
+)
 from rp_redcap.views import validate_project
+from sidekick.models import Organization
+
+
+def validate_organization(org_id, request):
+    try:
+        org = Organization.objects.get(id=org_id)
+    except Organization.DoesNotExist:
+        return status.HTTP_400_BAD_REQUEST
+
+    if not org.users.filter(id=request.user.id).exists():
+        return status.HTTP_401_UNAUTHORIZED
+
+    return status.HTTP_202_ACCEPTED
 
 
 class StartPatientDataCheckView(APIView):
@@ -13,5 +30,15 @@ class StartPatientDataCheckView(APIView):
         if return_status == status.HTTP_202_ACCEPTED:
             chain = create_hospital_groups.s() | patient_data_check.s()
             chain.delay(project_id, tz_code)
+
+        return HttpResponse(status=return_status)
+
+
+class StartScreeningRecordCheckView(APIView):
+    def post(self, request, org_id, *args, **kwargs):
+        return_status = validate_organization(org_id, request)
+
+        if return_status == status.HTTP_202_ACCEPTED:
+            screening_record_check.delay(org_id)
 
         return HttpResponse(status=return_status)
