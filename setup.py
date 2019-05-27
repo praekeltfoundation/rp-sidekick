d59354c73ef0821f9fd44fab8b6b37d991bd0136
Erik Harding <erik@praekelt.org>
2019-05-27 13:13:24 +0200

    Release 1.5.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rp-sidekick",
-    version="1.5.9",
+    version="1.5.10",
     url="http://github.com/praekeltfoundation/rp-sidekick",
     license="BSD",
     author="Praekelt Foundation",
