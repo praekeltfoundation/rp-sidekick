5739b6a58d00bec02771daf4d5d138297aa007e5
Kaitlyn Crawford <kaitlyn@praekelt.com>
2019-07-25 18:46:31 +0200

    Split out contact updating into a seperate task

--- rp_gpconnect/tests/test_task.py
+++ rp_gpconnect/tests/test_task.py
@@ -7,7 +7,7 @@ from django.test import TestCase, override_settings
 from openpyxl import Workbook
 
 from rp_gpconnect.models import ContactImport, trigger_contact_import
-from rp_gpconnect.tasks import process_contact_import
+from rp_gpconnect.tasks import import_or_update_contact, process_contact_import
 from sidekick.models import Organization
 from sidekick.tests.utils import assertCallMadeWith
 
@@ -47,94 +47,117 @@ class ProcessContactImportTaskTests(TestCase):
 
     @override_settings(MEDIA_ROOT=tempfile.gettempdir())
     @patch("rp_gpconnect.tasks.log")
-    @patch("temba_client.v2.TembaClient.get_contacts", autospec=True)
-    @patch("rp_gpconnect.tasks.get_whatsapp_contact_id", autospec=True)
-    def test_contact_import_skips_if_no_wa_id(
-        self, mock_get_whatsapp_contact_id, mock_get_rp_contact, mock_logger
+    @patch("rp_gpconnect.tasks.import_or_update_contact.delay")
+    def test_contact_import_calls_contact_task_for_each_row(
+        self, mock_contact_update_task, mock_logger
     ):
         temp_file = tempfile.NamedTemporaryFile(suffix=".xlsx")
-        contacts_file = create_temp_xlsx_file(temp_file, ["+27723456789"]).name
+        contacts_file = create_temp_xlsx_file(
+            temp_file, ["+27000000001", "+27000000002"]
+        ).name
         import_obj = ContactImport.objects.create(
             file=contacts_file, org=self.org, created_by=self.user
         )
 
-        mock_get_whatsapp_contact_id.return_value = None
-
         process_contact_import(import_obj.pk)
-        mock_get_whatsapp_contact_id.assert_called_with(self.org, "+27723456789")
-        mock_get_rp_contact.assert_not_called()
+
         mock_logger.info.assert_called_with(
-            "Skipping contact +27723456789. No WhatsApp Id."
+            "Importing contacts for file: %s" % contacts_file
         )
 
-    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+        self.assertEqual(mock_contact_update_task.call_count, 2)
+        mock_contact_update_task.assert_any_call(
+            {"msisdn": "+27000000001", "something_else": "stuuuuff"}, self.org.pk
+        )
+        mock_contact_update_task.assert_any_call(
+            {"msisdn": "+27000000002", "something_else": "stuuuuff"}, self.org.pk
+        )
+
+
+class ImportOrUpdateContactTaskTests(TestCase):
+    def setUp(self):
+        self.org = Organization.objects.create(
+            name="Test Organization", url="http://localhost:8002/", token="REPLACEME"
+        )
+        self.user = User.objects.create_user(
+            "username", "testuser@example.com", "password"
+        )
+
+    @patch("rp_gpconnect.tasks.log")
+    @patch("temba_client.v2.TembaClient.get_contacts", autospec=True)
+    @patch("rp_gpconnect.tasks.get_whatsapp_contact_id", autospec=True)
+    def test_contact_task_skips_if_no_wa_id(
+        self, mock_get_whatsapp_contact_id, mock_get_rp_contact, mock_logger
+    ):
+        mock_get_whatsapp_contact_id.return_value = None
+
+        import_or_update_contact(
+            {"msisdn": "+27000000001", "something_else": "stuuuuff"}, self.org.pk
+        )
+        mock_get_whatsapp_contact_id.assert_called_with(self.org, "+27000000001")
+        mock_get_rp_contact.assert_not_called()
+        mock_logger.info.assert_called_with(
+            "Skipping contact +27000000001. No WhatsApp Id."
+        )
+
     @patch("temba_client.v2.TembaClient.create_contact")
     @patch("temba_client.v2.TembaClient.update_contact")
     @patch("temba_client.v2.TembaClient.get_contacts")
     @patch("rp_gpconnect.tasks.get_whatsapp_contact_id")
-    def test_contact_import_if_contact_exists(
+    def test_contact_task_if_contact_exists(
         self,
         mock_get_whatsapp_contact_id,
         mock_get_rp_contact,
         mock_update_rp_contact,
         mock_create_rp_contact,
     ):
-        temp_file = tempfile.NamedTemporaryFile(suffix=".xlsx")
-        contacts_file = create_temp_xlsx_file(temp_file, ["+27723456789"]).name
-        import_obj = ContactImport.objects.create(
-            file=contacts_file, org=self.org, created_by=self.user
-        )
-
-        mock_get_whatsapp_contact_id.return_value = "27723456789"
+        mock_get_whatsapp_contact_id.return_value = "27000000001"
 
         mock_contact_object = Mock()
         mock_contact_object.uuid = "123456"
-        mock_contact_object.urns = ["tel:+27723456789"]
+        mock_contact_object.urns = ["tel:+27000000001"]
 
         mock_get_rp_contact.return_value.first.return_value = mock_contact_object
 
-        process_contact_import(import_obj.pk)
-        mock_get_whatsapp_contact_id.assert_called_with(self.org, "+27723456789")
+        import_or_update_contact(
+            {"msisdn": "+27000000001", "something_else": "stuuuuff"}, self.org.pk
+        )
+        mock_get_whatsapp_contact_id.assert_called_with(self.org, "+27000000001")
 
         self.assertEqual(mock_get_rp_contact.call_count, 1)
         mock_update_rp_contact.assert_called_with(
-            contact="123456", urns=["tel:+27723456789", "whatsapp:27723456789"]
+            contact="123456", urns=["tel:+27000000001", "whatsapp:27000000001"]
         )
 
         mock_create_rp_contact.assert_not_called()
 
-    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
     @patch("temba_client.v2.TembaClient.create_contact", autospec=True)
     @patch("temba_client.v2.TembaClient.update_contact", autospec=True)
     @patch("temba_client.v2.TembaClient.get_contacts", autospec=True)
     @patch("rp_gpconnect.tasks.get_whatsapp_contact_id", autospec=True)
-    def test_contact_import_if_new_contact(
+    def test_contact_task_if_new_contact(
         self,
         mock_get_whatsapp_contact_id,
         mock_get_rp_contact,
         mock_update_rp_contact,
         mock_create_rp_contact,
     ):
-        temp_file = tempfile.NamedTemporaryFile(suffix=".xlsx")
-        contacts_file = create_temp_xlsx_file(temp_file, ["+27723456789"]).name
-        import_obj = ContactImport.objects.create(
-            file=contacts_file, org=self.org, created_by=self.user
-        )
-
-        mock_get_whatsapp_contact_id.return_value = "27723456789"
+        mock_get_whatsapp_contact_id.return_value = "27000000001"
 
         mock_get_rp_contact.return_value.first.side_effect = [None, None]
 
-        process_contact_import(import_obj.pk)
+        import_or_update_contact(
+            {"msisdn": "+27000000001", "something_else": "stuuuuff"}, self.org.pk
+        )
 
         self.assertEqual(mock_get_rp_contact.call_count, 2)
         call_1_args, call_2_args = mock_get_rp_contact.call_args_list
-        assertCallMadeWith(call_1_args, urn="tel:+27723456789")
-        assertCallMadeWith(call_2_args, urn="whatsapp:27723456789")
+        assertCallMadeWith(call_1_args, urn="tel:+27000000001")
+        assertCallMadeWith(call_2_args, urn="whatsapp:27000000001")
 
         mock_update_rp_contact.assert_not_called()
 
         assertCallMadeWith(
             mock_create_rp_contact.call_args,
-            urns=["tel:+27723456789", "whatsapp:27723456789"],
+            urns=["tel:+27000000001", "whatsapp:27000000001"],
         )
