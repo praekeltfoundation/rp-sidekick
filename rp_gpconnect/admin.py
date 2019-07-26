f4e3f5684dcd6451bb3e44be27e332ffe0c26ba4
Kaitlyn Crawford <kaitlyn@praekelt.com>
2019-07-26 15:16:37 +0200

    Improve admin display of rp_gpconnect models

--- rp_gpconnect/admin.py
+++ rp_gpconnect/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
-from rp_gpconnect.models import ContactImport
+from rp_gpconnect.models import ContactImport, Flow
 
-admin.site.register(ContactImport)
+
+@admin.register(ContactImport)
+class ContactImportAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "file", "org", "created_by")
+    readonly_fields = ("created_at", "file", "org", "created_by")
+
+
+@admin.register(Flow)
+class FlowAdmin(admin.ModelAdmin):
+    list_display = ("type", "org", "rapidpro_flow")
