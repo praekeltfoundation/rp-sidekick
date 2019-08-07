6905dc07a341b64c1eb4ad2e60d608608c25a4a6
Erik Harding <erik@praekelt.org>
2019-08-07 09:56:42 +0200

    simplify clean_message

--- sidekick/utils.py
+++ sidekick/utils.py
@@ -1,4 +1,5 @@
 import json
+import re
 from urllib.parse import urljoin
 
 import pkg_resources
@@ -22,7 +23,7 @@ def get_current_week_number():
 
 
 def clean_message(message):
-    return " ".join(message.replace("\n", " ").replace("\t", " ").split()) or " "
+    return re.sub(r"\W+", " ", message)
 
 
 def clean_msisdn(msisdn):
