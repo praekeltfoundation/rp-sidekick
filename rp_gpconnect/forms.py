1fa4d534032f21cc355d0e3a8bd5764ab4ed883d
Kaitlyn Crawford <kaitlyn@praekelt.com>
2019-08-22 15:26:25 +0200

    Call import processing task from form or task not from post_save

--- rp_gpconnect/forms.py
+++ rp_gpconnect/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
 from rp_gpconnect.models import ContactImport
+from rp_gpconnect.tasks import process_contact_import
 
 
 class ContactImportForm(forms.ModelForm):
     class Meta:
         model = ContactImport
         fields = ["file", "org"]
+
+    def save(self, commit=True):
+        instance = super(ContactImportForm, self).save(commit=commit)
+        process_contact_import.delay(instance.pk)
+        return instance
