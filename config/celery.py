993da3bb4eb35f1cafbe4d8d6dbdf836e60b4a55
Buhle <buhle@praekelt.org>
2022-02-07 14:23:38 +0200

    changed celery environment settings

--- config/celery.py
+++ config/celery.py
@@ -6,9 +6,9 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("")
+app = Celery("config")
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
